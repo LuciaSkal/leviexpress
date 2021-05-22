c0de0d76d9df824989b2934deaaa7bed10e495c8
Lucia Skalinova <skalinovalucia@gmail.com>
2021-05-22 11:05:57 +0200

    routovani dodelano

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 import { render } from 'react-dom';
 import Footer from './Footer';
 import Header from './Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import './style.css';
+import Home from './pages/Home'
+import Reservation from './pages/Reservation'
 
 const App = () => {
 
   return (
-    <>
+    <Router>
      <Header/>
-
-     <Footer/>
-    </>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/reservation" exact>
+           <Reservation />
+        </Route>
+      </Switch>
+    <Footer/>
+  </Router>
   )
 }
 
---
+++ src/pages/Reservation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Reservation = () => {
+
+  return(
+    <h2>Rezervujte si</h2>
+  )
+}
+
+export default Reservation
