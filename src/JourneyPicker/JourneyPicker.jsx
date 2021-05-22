cb9f4829f92a20bb0ad038bd08177f543bbdf5fb
Lucia Skalinova <skalinovalucia@gmail.com>
2021-05-22 12:10:31 +0200

    formular na vyber napojeni

--- src/JourneyPicker/JourneyPicker.jsx
+++ src/JourneyPicker/JourneyPicker.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import mapImage from './img/map.svg';
 import './style.css';
 
 const JourneyPicker = () => {
+
+  
+    const [fromCity, setFromCity] = useState('')
+    const [toCity, setToCity] = useState('')
+    const [date, setDate] = useState('')
+
+
+    const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Odesílám formulář s cestou ${fromCity}, ${toCity}, ${date} `)
+    }
+    
+    const onSelectFromCity = (event) => {
+      setFromCity(event.target.value)
+    }
+    const onSelectToCity = (event) => {
+      setToCity(event.target.value)
+    }
+
+    const onSelectDate = (event) => {
+      setDate(event.target.value)
+    }
+
 
   return (
      <div className="journey-picker container">
@@ -11,7 +34,7 @@ const JourneyPicker = () => {
        <form className="journey-picker__form">
          <label>
            <div className="journey-picker__label">Odkud:</div>
-           <select>
+           <select value={fromCity} onChange={onSelectFromCity}>
              <option value="">Vyberte</option>
              <option value="Mesto1">Město 1</option>
              <option value="Mesto2">Město 2</option>
@@ -21,7 +44,7 @@ const JourneyPicker = () => {
          </label>
          <label>
            <div className="journey-picker__label">Kam:</div>
-           <select>
+           <select value={toCity} onChange={onSelectToCity}>
              <option value="">Vyberte</option>
              <option value="Mesto1">Město 1</option>
              <option value="Mesto2">Město 2</option>
@@ -31,7 +54,7 @@ const JourneyPicker = () => {
          </label>
          <label>
            <div className="journey-picker__label">Datum:</div>
-           <select>
+           <select value={date} onChange={onSelectDate}>
              <option value="">Vyberte</option>
              <option>20.05.2021</option>
              <option>21.05.2021</option>
@@ -40,7 +63,8 @@ const JourneyPicker = () => {
            </select>
          </label>
          <div className="journey-picker__controls">
-           <button className="btn" type="submit">Vyhledat spoj</button>
+           <button onClick={handleSubmit}
+            className="btn" type="submit">Vyhledat spoj</button>
          </div>
        </form>
        <img src={mapImage} />
